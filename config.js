a1df201a83ef992e7f4a0d04f3f92144a68f6e99
oguiller <grodrigu@AML1459.eu.acncorp.com>
2015-11-22 19:11:16 +0100

    Added url to config options per environment

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ module.exports = function(){
 	console.log("Environment SET to: " + process.env.NODE_ENV)
     switch(process.env.NODE_ENV){
         case 'development':
-            return {'database': 'mongodb://localhost/wind'};
+            return {'database': 'mongodb://localhost/wind', 'url':'http://localhost'};
 
         case 'production':
-            return {'database': 'mongodb://heroku_6v8t7pqh:heroku_6v8t7pqh@ds047114.mongolab.com:47114/heroku_6v8t7pqh'};
+            return {'database': 'mongodb://heroku_6v8t7pqh:heroku_6v8t7pqh@ds047114.mongolab.com:47114/heroku_6v8t7pqh', 'url':'https://ascl.herokuapp.com/'};
 
         default:
-            return {'database': 'mongodb://localhost/wind'};
+            return {'database': 'mongodb://localhost/wind', 'url':'http://localhost'};
     }
 };
