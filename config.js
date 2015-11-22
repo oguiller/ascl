be22756457de8ccbdf71225f6ba85c0faf42eb25
oguiller <grodrigu@AML1459.eu.acncorp.com>
2015-11-22 18:37:01 +0100

    Adding properties for different environments

--- config.js
+++ config.js
@@ -1,3 +1,13 @@
-module.exports = {
-    'database': 'mongodb://localhost/wind'
+module.exports = function(){
+	console.log("Environment SET to: " + process.env.NODE_ENV)
+    switch(process.env.NODE_ENV){
+        case 'development':
+            return {'database': 'mongodb://localhost/wind'};
+
+        case 'production':
+            return {'database': 'mongodb://heroku_6v8t7pqh:heroku_6v8t7pqh@ds047114.mongolab.com:47114/heroku_6v8t7pqh'};
+
+        default:
+            return {'database': 'mongodb://localhost/wind'};
+    }
 };
--- server.js
+++ server.js
@@ -14,8 +14,8 @@ var measurement = require('./models/measurement');
 var  measurementController = require('./controllers/measurement');
 
 // used to create, sign, and verify tokens
-var config = require('./config'); // get our config file
-
+var Config = require('./config'), // get our config file
+	conf = new Config();	
 /*
  Temporary solution to keep the server running -- This is actually an anti-pattern
  should be solved with a Domain
@@ -26,7 +26,7 @@ process.on('uncaughtException', function(err) {
 });
 
 var port = process.env.PORT || 8080; 
-mongoose.connect(config.database); // connect to database
+mongoose.connect(conf.database); // connect to database
 console.log('Server is listening on port ' + port);
 
 // use body parser so we can get info from POST and/or URL parameters
